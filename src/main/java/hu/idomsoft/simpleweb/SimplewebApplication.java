a80288bab3311ef413fb9b40fcfe6c6c2e714ab2
Sóti Csaba <csaba.soti@idomsoft.hu>
2023-06-12 13:51:06 +0200

    Init

---
+++ src/main/java/hu/idomsoft/simpleweb/SimplewebApplication.java
@@ -0,0 +1,13 @@
+package hu.idomsoft.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebApplication.class, args);
+	}
+
+}
